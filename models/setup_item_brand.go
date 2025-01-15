322d17e3276d82aa918964e8cf3a7f8cd45003ac
Pierce Perado <pierceperado@lifesaver.care>
2025-01-15 15:43:31 +0800

    fixed get brands with at

--- models/setup_item_brand.go
+++ models/setup_item_brand.go
@@ -1,34 +1,13 @@
 package models
 
-import (
-	"fmt"
-
-	"gorm.io/gorm"
-)
-
 type Brand struct {
 	ID   uint   `gorm:"primarykey" json:"id"`
 	Code string `json:"code"`
 	Name string `json:"name"`
-	At   `gorm:"-"`
 }
 
 func (Brand) TableName() string {
 	return "tbl_setup_item_brand"
-}
-
-func (b *Brand) AfterSave(tx *gorm.DB) (err error) {
-	fmt.Println("Brand Result:", b)
-	brandAt := BrandAt{
-		Brand: *b,
-		At:    b.At,
-	}
-
-	if err := tx.Create(&brandAt).Error; err != nil {
-		return err
-	}
-
-	return nil
 }
 
 type BrandAt struct {
