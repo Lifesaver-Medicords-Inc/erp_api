e331436a2d6750691cfe54c37789323cfddfdc70
unknown <intern2@lifesaver.care>
2025-01-22 18:01:24 +0800

    applications handler

---
+++ handlers/sales_handlers/applications_handler.go
@@ -0,0 +1,129 @@
+package sales_handlers
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/pierceperado/smpc/initializers"
+	"github.com/pierceperado/smpc/services/sales_services"
+)
+
+// handles the http requests for the services
+func GetApplications(c *fiber.Ctx) error {
+	data, err := sales_services.GetApplications()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data":    data,
+	})
+
+}
+
+// specific http request based on id
+func GetApplication(c *fiber.Ctx) error {
+	idParam := c.Params("id")
+
+	idNum, err := strconv.Atoi(idParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
+
+	data, err := sales_services.GetApplication(idNum)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data":    data,
+	})
+}
+
+// handles http request for Creating application data
+func CreateApplication(c *fiber.Ctx) error {
+	tx := initializers.DB.Begin()
+	err := sales_services.CreateApplication(c, tx)
+
+	if err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed creating application",
+		})
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed to commit transaction",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+	})
+}
+
+func UpdateApplication(c *fiber.Ctx) error {
+	tx := initializers.DB.Begin()
+	err := sales_services.UpdateApplication(c, tx)
+
+	if err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed updating application",
+		})
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed to commit transaction",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+	})
+}
+
+func DeleteApplication(c *fiber.Ctx) error {
+	tx := initializers.DB.Begin()
+	err := sales_services.DeleteApplication(c, tx)
+
+	if err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed deleting application",
+		})
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed to commit transaction",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+	})
+}
