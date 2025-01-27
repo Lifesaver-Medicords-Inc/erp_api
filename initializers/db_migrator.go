9743d4fb2d35d01675207652e88583bc2f396192
KristoferDauigoy18 <intern3@lifesaver.care>
2025-01-27 17:10:00 +0800

    merge main to kristofer_branch

--- initializers/db_migrator.go
+++ initializers/db_migrator.go
@@ -5,14 +5,15 @@ import "github.com/pierceperado/smpc/models"
 func MigrateDb() {
 	// Auth
 
-	DB.AutoMigrate(&models.User{}, &models.UserAt{})
-	// Setup
-	DB.AutoMigrate(&models.Brand{}, &models.BrandAt{})
-	DB.AutoMigrate(&models.UnitMeasurement{}, &models.UnitMeasurementAt{})
-	DB.AutoMigrate(&models.PaymentTerms{}, &models.PaymentTermsAt{})
-	DB.AutoMigrate(&models.Social{}, &models.SocialAt{})
-	DB.AutoMigrate(&models.Entity{}, &models.EntityAt{})
-	DB.AutoMigrate(&models.Industries{}, &models.IndustriesAt{})
+	// DB.AutoMigrate(&models.User{}, &models.UserAt{})
+	// // Setup
+	// DB.AutoMigrate(&models.Brand{}, &models.BrandAt{})
+	// DB.AutoMigrate(&models.UnitMeasurement{}, &models.UnitMeasurementAt{})
+	// DB.AutoMigrate(&models.PaymentTerms{}, &models.PaymentTermsAt{})
+	// DB.AutoMigrate(&models.Social{}, &models.SocialAt{})
+	// DB.AutoMigrate(&models.Entity{}, &models.EntityAt{})
+	// DB.AutoMigrate(&models.Industries{}, &models.IndustriesAt{})
+	DB.AutoMigrate(&models.ShipType{}, &models.ShipTypeAt{})
 	// DB.AutoMigrate(&models.Class{}, &models.ClassAt{})
 	// DB.AutoMigrate(&models.Name{}, &models.NameAt{})
 	// DB.AutoMigrate(&models.Type{}, &models.TypeAt{})
