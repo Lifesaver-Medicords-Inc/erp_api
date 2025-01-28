9530b82676c70d63b4a3dd2014c1cafe3245f2b5
jacinth0727 <intern2@lifesaver.care>
2025-01-28 14:29:31 +0800

    commented the other migrators to prevent nuking other tables

--- initializers/db_migrator.go
+++ initializers/db_migrator.go
@@ -7,15 +7,17 @@ func MigrateDb() {
 	DB.AutoMigrate(&models.User{}, &models.UserAt{})
 
 	// Setup
-	DB.AutoMigrate(&models.Brand{}, &models.BrandAt{})
-	DB.AutoMigrate(&models.UnitMeasurement{}, &models.UnitMeasurementAt{})
-	DB.AutoMigrate(&models.PaymentTerms{}, &models.PaymentTermsAt{})
-	DB.AutoMigrate(&models.Class{}, &models.ClassAt{})
-	DB.AutoMigrate(&models.Name{}, &models.NameAt{})
-	DB.AutoMigrate(&models.Type{}, &models.TypeAt{})
-	DB.AutoMigrate(&models.Item{}, &models.ItemAt{})
-	DB.AutoMigrate(&models.Model{}, &models.ModelAt{})
+	// DB.AutoMigrate(&models.Brand{}, &models.BrandAt{})
+	// DB.AutoMigrate(&models.UnitMeasurement{}, &models.UnitMeasurementAt{})
+	// DB.AutoMigrate(&models.PaymentTerms{}, &models.PaymentTermsAt{})
+	// DB.AutoMigrate(&models.Class{}, &models.ClassAt{})
+	// DB.AutoMigrate(&models.Name{}, &models.NameAt{})
+	// DB.AutoMigrate(&models.Type{}, &models.TypeAt{})
+	// DB.AutoMigrate(&models.Item{}, &models.ItemAt{})
+	// DB.AutoMigrate(&models.Model{}, &models.ModelAt{})
+	// DB.AutoMigrate(&models.Application{}, &models.ApplicationAt{})
 
 	// Sales
 	DB.AutoMigrate(&models.SalesQuotation{}, &models.SalesQuotationAt{})
+	DB.AutoMigrate(&models.SalesQuotationQuick{}, &models.SalesQuotationQuickAt{})
 }
