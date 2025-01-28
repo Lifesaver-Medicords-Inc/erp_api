1a7ba2e0f72b0a249c87a71a8b58226e5a454700
jacinth0727 <intern2@lifesaver.care>
2025-01-28 14:25:52 +0800

    application handler

--- handlers/setup_handlers/application_handler.go
+++ handlers/setup_handlers/application_handler.go
@@ -1,33 +1,32 @@
-package sales_handlers
+package setup_handlers
 
 import (
-	"fmt"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/pierceperado/smpc/initializers"
-	"github.com/pierceperado/smpc/services/sales_services"
+	"github.com/pierceperado/smpc/services/setup_services"
 	"github.com/pierceperado/smpc/utils"
 )
 
-func GetSalesQuotations(c *fiber.Ctx) error {
-	data, status, err := sales_services.GetSalesQuotations(nil)
+// TWO HANDLERS FOR GETTING APPLICATIONS
+func GetApplications(c *fiber.Ctx) error {
+	data, status, err := setup_services.GetApplications(nil)
 	if err != nil {
 		return utils.RespondError(c, status, err.Error())
 	}
 
 	return utils.RespondSuccess(c, data)
-
 }
 
-func GetSalesQuotation(c *fiber.Ctx) error {
+func GetApplication(c *fiber.Ctx) error {
 	idParam := c.Params("id")
 	idNum, err := strconv.Atoi(idParam)
 	if err != nil {
 		return utils.RespondError(c, fiber.StatusBadRequest, err.Error())
 	}
 
-	data, status, err := sales_services.GetSalesQuotation(idNum)
+	data, status, err := setup_services.GetApplication(idNum)
 	if err != nil {
 		return utils.RespondError(c, status, err.Error())
 	}
@@ -35,32 +34,15 @@ func GetSalesQuotation(c *fiber.Ctx) error {
 	return utils.RespondSuccess(c, data)
 }
 
-func CreateSalesQuotation(c *fiber.Ctx) error {
+///////////////////////////////
+
+// HANDLER FOR CREATING APPLICATION
+func CreateApplication(c *fiber.Ctx) error {
 	tx := initializers.DB.Begin()
 	if tx.Error != nil {
 		return utils.RespondError(c, fiber.StatusInternalServerError, "Failed to start transaction")
 	}
-	data, status, err := sales_services.CreateSalesQuotation(c, tx)
-	if err != nil {
-		tx.Rollback()
-		return utils.RespondError(c, status, err.Error())
-	}
-
-	if err := tx.Commit().Error; err != nil {
-		tx.Rollback()
-		return utils.RespondError(c, fiber.StatusInternalServerError, "Failed to commit transaction")
-	}
-
-	return utils.RespondSuccess(c, data)
-}
-
-func UpdateSalesQuotation(c *fiber.Ctx) error {
-	tx := initializers.DB.Begin()
-	if tx.Error != nil {
-		return utils.RespondError(c, fiber.StatusInternalServerError, "Failed to start transaction")
-	}
-
-	data, status, err := sales_services.UpdateSalesQuotation(c, tx, nil)
+	data, status, err := setup_services.CreateApplication(c, tx)
 	if err != nil {
 		tx.Rollback()
 		return utils.RespondError(c, status, err.Error())
@@ -74,14 +56,35 @@ func UpdateSalesQuotation(c *fiber.Ctx) error {
 	return utils.RespondSuccess(c, data)
 }
 
-func DeleteSalesQuotation(c *fiber.Ctx) error {
+// HANDLER FOR UPDATING APPLICATION
+func UpdateApplication(c *fiber.Ctx) error {
 	tx := initializers.DB.Begin()
 	if tx.Error != nil {
 		return utils.RespondError(c, fiber.StatusInternalServerError, "Failed to start transaction")
 	}
 
-	data, status, err := sales_services.DeleteSalesQuotation(c, tx, nil)
-	fmt.Println("DATAAA: ", data)
+	data, status, err := setup_services.UpdateApplication(c, tx, nil)
+	if err != nil {
+		tx.Rollback()
+		return utils.RespondError(c, status, err.Error())
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return utils.RespondError(c, fiber.StatusInternalServerError, "Failed to commit transaction")
+	}
+
+	return utils.RespondSuccess(c, data)
+}
+
+// HANDLER DELETE APPLICATION
+func DeleteApplication(c *fiber.Ctx) error {
+	tx := initializers.DB.Begin()
+	if tx.Error != nil {
+		return utils.RespondError(c, fiber.StatusInternalServerError, "Failed to start transaction")
+	}
+
+	data, status, err := setup_services.DeleteApplication(c, tx, nil)
 	if err != nil {
 		tx.Rollback()
 		return utils.RespondError(c, status, err.Error())
