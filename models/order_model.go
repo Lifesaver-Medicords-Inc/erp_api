88ca198fd7923efea5addc592473065f2effeb45
KristoferDauigoy18 <intern3@lifesaver.care>
2025-02-12 18:09:36 +0800

    order and details model refractor

--- models/order_model.go
+++ models/order_model.go
@@ -1,28 +1,33 @@
 package models
 
 type OrderContent struct {
-	CustomerName   string `json:"customer_name"`
-	CustomerCode   string `json:"customer_code"`
-	ShipTo         string `json:"ship_to"`
-	BillTo         string `json:"bill_to"`
-	Doc            string `json:"doc"`
-	Date           string `json:"date"`
-	DeliveryDate   string `json:"delivery_date"`
-	Payment_Terms  string `json:"payment_terms"`
-	Ship_Type      string `json:"ship_type"`
-	Document_No    string `json:"document_no"`
-	Ref_Id         uint   `json:"ref_id"`
-	Status         string `json:"status"`
-	SalesExecutive string `json:"sales_executive"`
-	Receiver       string `json:"receiver"`
-	ContactNo      string `json:"contact_no"`
-	Remarks        string `json:"remarks"`
-	Vat            string `json:"vat"`
-	NetofVat       string `json:"netof_vat"`
-	TotalAmountDue string `json:"total_amount_due"`
-	ApprovedBy     string `json:"approved_by"`
-	ApprovedByID   uint   `json:"approved_by_id"`
-	Tin            string `json:"tin"`
+	Ship_Type_ID     uint   `json:"ship_type_id"`
+	Bill_To_ID       uint   `json:"bill_to_id"`
+	Ship_To_ID       uint   `json:"ship_to_id"`
+	Customer_ID      uint   `json:"customer_id"`
+	Quotation_ID     uint   `json:"quotation_id"`
+	ApprovedBy       string `json:"approved_by"`
+	ApprovedByID     uint   `json:"approved_by_id"`
+	Doc              string `json:"doc"`
+	Date             string `json:"date"`
+	DeliveryDate     string `json:"delivery_date"`
+	Payment_Terms_ID string `json:"payment_terms_id"`
+	Document_No      string `json:"document_no"`
+	Status           string `json:"status"`
+	// CustomerName  string `json:"customer_name"`
+	// CustomerCode  string `json:"customer_code"`
+	// ShipTo        string `json:"ship_to"`
+	// BillTo        string `json:"bill_to"`
+	// SalesExecutive string `json:"sales_executive"`
+	// Receiver       string `json:"receiver"`
+	// ContactNo      string `json:"contact_no"`
+	// Remarks        string `json:"remarks"`
+	// Vat            string `json:"vat"`
+	// NetofVat       string `json:"netof_vat"`
+	// TotalAmountDue string `json:"total_amount_due"`
+	// ApprovedBy     string `json:"approved_by"`
+	// ApprovedByID   uint   `json:"approved_by_id"`
+	// Tin            string `json:"tin"`
 }
 
 type Order struct {
