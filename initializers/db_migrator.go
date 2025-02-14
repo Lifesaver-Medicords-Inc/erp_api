47b390fce67dc6ab309a0461d6c689d2a67a3a82
Pierce Perado <pierceperado@lifesaver.care>
2025-02-14 14:45:11 +0800

    added websocket and file uploader

--- initializers/db_migrator.go
+++ initializers/db_migrator.go
@@ -71,5 +71,4 @@ func MigrateDb() {
 	// DB.AutoMigrate(&models.BpiEntity{}, &models.BpiEntityAt{})
 	// DB.AutoMigrate(&models.BpiAddress{}, &models.BpiAddressAt{})
 	// DB.AutoMigrate(&models.BpiItems{}, &models.BpiItemsAt{})
-
 }
