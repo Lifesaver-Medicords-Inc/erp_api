22bb1fb00790ef0a0464406fbbe208339b2c24c1
KristoferDauigoy18 <intern3@lifesaver.care>
2025-02-18 17:40:45 +0800

    order model and order/details services update

--- initializers/db_migrator.go
+++ initializers/db_migrator.go
@@ -38,9 +38,9 @@ func MigrateDb() {
 	// DB.AutoMigrate(&models.Opportunity{}, &models.OpportunityAt{})
 	// // DB.AutoMigrate(&models.SalesQuotation{}, &models.SalesQuotationAt{})
 	// // DB.AutoMigrate(&models.SalesQuotationQuick{}, &models.SalesQuotationQuickAt{})
-	//DB.AutoMigrate(&models.Order{}, &models.OrderAt{})
+	// DB.AutoMigrate(&models.Order{}, &models.OrderAt{})
 
-	//DB.AutoMigrate(&models.OrderDetails{}, &models.OrderDetailsAt{})
+	// DB.AutoMigrate(&models.OrderDetails{}, &models.OrderDetailsAt{})
 	// DB.AutoMigrate(&models.Opportunity{}, &models.OpportunityAt{})
 	// DB.AutoMigrate(&models.Status{}, &models.StatusAt{})
 	// DB.AutoMigrate(&models.SalesQuotation{}, &models.SalesQuotationAt{})
