e636afb46a3c596d7f811d58d477ca1830f7cf05
jerome-obogne-lifesaver <jeromeobogne@lifesaver.care>
2025-07-07 19:00:26 +0800

    features(Tax Code Setup): Add tax code models

---
+++ models/accounting_models/accounting_tax_details_model.go
@@ -0,0 +1,27 @@
+package accounting_models
+import "github.com/pierceperado/smpc/models"
+
+type TaxDetailsContent struct {
+	TaxCodeId uint    `json:"tax_code_id"`
+	ValidFrom string  `json:"valid_from"`
+	ValidTo   string  `json:"valid_to"`
+	TaxRate   float64 `json:"tax_rate"`
+}
+type TaxDetails struct {
+	ID uint `gorm:"primarykey" json:"id"`
+	TaxDetailsContent
+}
+func (TaxDetails) TableName() string {
+	return "tbl_setup_tax_details"
+}
+
+type TaxDetailsAt struct {
+	ID    uint `gorm:"primarykey" json:"id"`
+	RefId uint `json:"ref_id"`
+	TaxDetailsContent
+	models.At
+}
+
+func (TaxDetailsAt) TableName() string {
+	return "z_tbl_setup_tax_details_at"
+}
---
+++ models/accounting_models/accounting_tax_model.go
@@ -0,0 +1,32 @@
+package accounting_models
+
+import "github.com/pierceperado/smpc/models"
+
+type TaxContent struct {
+	TaxDesc                   string `json:"tax_desc"`
+	InputTaxCreditable        *bool  `json:"input_tax_creditable"`
+	InputTaxAccountSalesId    uint   `json:"input_tax_account_sales_id"`
+	InputTaxAccountPurchaseId uint   `json:"input_tax_account_purchase_id"`
+	Remarks                   string `json:"remarks"`
+}
+
+type Tax struct {
+	ID   uint   `gorm:"primarykey" json:"id"`
+	Code string `gorm:"unique;size:100" json:"code"`
+	TaxContent
+}
+
+func (Tax) TableName() string {
+	return "tbl_setup_tax"
+}
+
+type TaxAt struct {
+	ID    uint `gorm:"primarykey" json:"id"`
+	RefId uint `json:"ref_id"`
+	TaxContent
+	models.At
+}
+
+func (TaxAt) TableName() string {
+	return "z_tbl_setup_tax_at"
+}
