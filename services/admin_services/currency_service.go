4ef04d2ef3854d4843da66a58e11e81d9fd64a79
Igiee <igiebaldesanso@lifesaver.care>
2025-09-29 09:52:09 +0800

    feat(api): add CurrencyHandler, CurrencyRoute, CurrencyServices

---
+++ services/admin_services/currency_service.go
@@ -0,0 +1,44 @@
+package adminservices
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type CurrencyService struct {
+}
+
+func NewCurrencyService() *CurrencyService {
+	return &CurrencyService{}
+}
+
+func (c *CurrencyService) GetCurrencyAPI(baseCode string) (currencyBody string) {
+
+	url := "https://api.fxratesapi.com/latest?base=PHP"
+
+	if baseCode != "" {
+		url = "https://api.fxratesapi.com/latest?base=" + baseCode
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	return string(body)
+}
