4d1f10b26fcee07d51802002f07e405055288467
Igiee <igiebaldesanso@lifesaver.care>
2025-10-23 18:19:42 +0800

    feature(quick quotation): added shortDescriptions, ReferenceCode then delete IsChild

--- models/sales_quotation_quick.go
+++ models/sales_quotation_quick.go
@@ -2,20 +2,23 @@ package models
 
 type SalesQuotationQuickContent struct {
 	// Parent ID references to Sales Quotation Model
-	IsChild         bool    `json:"is_child"`
-	BasedId         uint    `json:"based_id"`
-	BomId           uint    `json:"bom_id"`
-	ItemId          uint    `json:"item_id"`
-	Components      string  `json:"components"`
-	Model           string  `json:"model"`
-	Qty             uint    `json:"qty"`
-	UnitOfMeasure   string  `json:"unit_of_measure"`
-	ListPrice       float64 `json:"list_price"`
-	UnitPrice       float64 `json:"unit_price"`
-	PercentDiscount string  `json:"percent_discount"`
-	NetDiscount     float64 `json:"net_discount"`
-	NetTotal        float64 `json:"net_total"`
-	LineTotal       float64 `json:"line_total"`
+	BasedId          uint    `json:"based_id"`
+	BomId            uint    `json:"bom_id"`
+	ItemId           uint    `json:"item_id"`
+	ReferenceCode    string  `json:"reference_code"`
+	Components       string  `json:"components"`
+	Model            string  `json:"model"`
+	Qty              uint    `json:"qty"`
+	ManDays          uint    `json:"man_days"`
+	LaborRate        uint    `json:"labor_rate"`
+	UnitOfMeasure    string  `json:"unit_of_measure"`
+	ListPrice        float64 `json:"list_price"`
+	UnitPrice        float64 `json:"unit_price"`
+	PercentDiscount  string  `json:"percent_discount"`
+	NetDiscount      float64 `json:"net_discount"`
+	NetTotal         float64 `json:"net_total"`
+	LineTotal        float64 `json:"line_total"`
+	ShortDescription string  `json:"short_description"`
 }
 
 type SalesQuotationQuick struct {
