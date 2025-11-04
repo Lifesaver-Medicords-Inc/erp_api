9244350ab16670f771c185ec10600a79c2323bfa
Igiee <igiebaldesanso@lifesaver.care>
2025-11-04 10:06:13 +0800

    chores (migration DB): return the comment

--- initializers/db_migrator.go
+++ initializers/db_migrator.go
@@ -81,7 +81,7 @@ func MigrateDb() {
 	// DB.AutoMigrate(&models.OrderDetails{}, &models.OrderDetailsAt{})
 	// DB.AutoMigrate(&models.Opportunity{}, &models.OpportunityAt{})
 
-	DB.AutoMigrate(&models.SalesQuotation{}, &models.SalesQuotationAt{})
+	//DB.AutoMigrate(&models.SalesQuotation{}, &models.SalesQuotationAt{})
 	//DB.AutoMigrate(&models.SalesQuotationQuick{}, &models.SalesQuotationQuickAt{})
 
 	// DB.AutoMigrate(&models.Order{}, &models.OrderAt{})
