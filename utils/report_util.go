b74d8f32340b5eb437f8b740badb717f0d1d0f4f
lumabanls <intern1@lifesaver.care>
2026-02-24 09:26:28 +0800

    feature(report_util): Add next doc number generator upon record creation

--- utils/report_util.go
+++ utils/report_util.go
@@ -1,8 +1,36 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 func DocNoGenerator(id uint) string {
 	DocNo := fmt.Sprintf( /**"RR#" + **/ "%04d", id)
 	return DocNo
+}
+
+// NextDocNo generates the next document number
+func NextDocNo(tx *gorm.DB, model any, column string) (int, error) {
+	var last int
+
+	// Safely parse model to get table name
+	stmt := &gorm.Statement{DB: tx}
+	if err := stmt.Parse(model); err != nil {
+		return 0, err
+	}
+
+	tableName := stmt.Schema.Table
+
+	query := fmt.Sprintf(`
+		SELECT COALESCE(MAX(%s), 0)
+		FROM %s WITH (UPDLOCK, HOLDLOCK)
+	`, column, tableName)
+
+	if err := tx.Raw(query).Scan(&last).Error; err != nil {
+		return 0, err
+	}
+
+	return last + 1, nil
 }
