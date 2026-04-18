0dfcd8626c4ff737295caf7a8c457ea70316f7ff
lumabanls <intern1@lifesaver.care>
2026-04-18 16:56:41 +0800

    hotfix(delivery receipt): add sales_order_details_id column

--- models/dispatching_model/delivery_receipt_items_model.go
+++ models/dispatching_model/delivery_receipt_items_model.go
@@ -3,13 +3,14 @@ package dispatching_models
 import "github.com/pierceperado/smpc/models"
 
 type DeliveryReceiptItemsContent struct {
-	DeliveryReceiptID uint   `gorm:"not null;index" json:"delivery_receipt_id"`
-	ItemID            uint   `gorm:"not null" json:"item_id"`
-	Qty               int    `gorm:"not null" json:"qty"`
-	UnitOfMeasure     string `gorm:"not null" json:"unit_of_measure"`
-	ItemCode          string `gorm:"size:50" json:"item_code,omitempty"`
-	ItemDescription   string `gorm:"size:255" json:"item_description,omitempty"`
-	SerialNo          string `gorm:"size:100" json:"serial_no,omitempty"`
+	DeliveryReceiptID   uint   `gorm:"not null;index" json:"delivery_receipt_id"`
+	SalesOrderDetailsId uint   `gorm:"not null" json:"sales_order_details_id"`
+	ItemID              uint   `gorm:"not null" json:"item_id"`
+	Qty                 int    `gorm:"not null" json:"qty"`
+	UnitOfMeasure       string `gorm:"not null" json:"unit_of_measure"`
+	ItemCode            string `gorm:"size:50" json:"item_code,omitempty"`
+	ItemDescription     string `gorm:"size:255" json:"item_description,omitempty"`
+	SerialNo            string `gorm:"size:100" json:"serial_no,omitempty"`
 }
 
 type DeliveryReceiptItems struct {
